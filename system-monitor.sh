2d474882ef8b67792f22fd7f2e3ddcae3d3db7d1
Sid <sidmukherjee@codethink.co.uk>
2026-07-23 13:57:48 +0100

    Added more of a dashboard functionality and separated info into related groups.

--- system-monitor.sh
+++ system-monitor.sh
@@ -5,18 +5,39 @@ hostname=$(hostname)
 username=$(whoami)
 uptime=$(uptime -p)
 diskUsage=$(df -h / | awk 'NR==2 {print $5}')
+cpuIdle=$(top -bn1 | grep "Cpu(s)" | awk '{print $8}')
+cpuUsage=$(echo "100 - $cpuIdle" | bc)  
+ramUsage=$(free -h --giga | grep "Mem" | awk '{print $3}')
+batteryPercent=$(cat /sys/class/power_supply/BAT0/capacity)
+ipAddress=$(ip route get 1.1.1.1 | awk '{print $7}')
 
 echo "============================"
 echo "System Health Dashboard"
 echo "============================"
 echo 
-echo "Today's date: $today" 
-echo 
-echo "Hostname: $hostname" 
+echo "General"
+echo "------------------------------------"
 echo
-echo "Username: $username"
+echo "Today's date:   $today" 
 echo 
-echo "Uptime: $uptime"
+echo "Hostname:       $hostname" 
 echo
-echo "Disk usage: $diskUsage"
-
+echo "Username:       $username"
+echo 
+echo "Uptime:         $uptime"
+echo
+echo "Resources"
+echo "------------------------------------"
+echo
+echo "Disk usage:     $diskUsage"
+echo
+echo "CPU Usage:      $cpuUsage%" 
+echo 
+echo "Memory Usage:   $ramUsage"
+echo 
+echo "Battery:        $batteryPercent%"  
+echo
+echo "Network"
+echo "------------------------------------"
+echo 
+echo "IP address:     $ipAddress"
