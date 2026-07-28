77b6b8f2c5fa834fef2713de57f67a45598eb053
Sid <sidmukherjee@codethink.co.uk>
2026-07-28 14:20:41 +0100

    Refactored dashboard into functions and added a progress bar alongside colours for battery percentage.

--- system-monitor.sh
+++ system-monitor.sh
@@ -1,43 +1,96 @@
 #!/bin/bash
 
-today=$(date)
-hostname=$(hostname)
-username=$(whoami)
-uptime=$(uptime -p)
-diskUsage=$(df -h / | awk 'NR==2 {print $5}')
-cpuIdle=$(top -bn1 | grep "Cpu(s)" | awk '{print $8}')
-cpuUsage=$(echo "100 - $cpuIdle" | bc)  
-ramUsage=$(free -h --giga | grep "Mem" | awk '{print $3}')
-batteryPercent=$(cat /sys/class/power_supply/BAT0/capacity)
-ipAddress=$(ip route get 1.1.1.1 | awk '{print $7}')
+getSysInfo() { 
+    today=$(date)
+    hostname=$(hostname)
+    username=$(whoami)
+    uptime=$(uptime -p)
+    diskUsage=$(df -h / | awk 'NR==2 {print $5}')
+    cpuIdle=$(top -bn1 | grep "Cpu(s)" | awk '{print $8}')
+    cpuUsage=$(echo "100 - $cpuIdle" | bc)  
+    ramUsage=$(free -h --giga | grep "Mem" | awk '{print $3}')
+    batteryPercent=$(cat /sys/class/power_supply/BAT0/capacity)
+    ipAddress=$(ip route get 1.1.1.1 | awk '{print $7}')
+}
 
-echo "============================"
-echo "System Health Dashboard"
-echo "============================"
-echo 
-echo "General"
-echo "------------------------------------"
-echo
-echo "Today's date:   $today" 
-echo 
-echo "Hostname:       $hostname" 
-echo
-echo "Username:       $username"
-echo 
-echo "Uptime:         $uptime"
-echo
-echo "Resources"
-echo "------------------------------------"
-echo
-echo "Disk usage:     $diskUsage"
-echo
-echo "CPU Usage:      $cpuUsage%" 
-echo 
-echo "Memory Usage:   $ramUsage"
-echo 
-echo "Battery:        $batteryPercent%"  
-echo
-echo "Network"
-echo "------------------------------------"
-echo 
-echo "IP address:     $ipAddress"
+printHeader() {
+    echo "============================"
+    echo "System Health Dashboard"
+    echo "============================"
+}
+
+printGeneralInfo() {
+    echo "Today's date:   $today" 
+    echo 
+    echo "Hostname:       $hostname" 
+    echo
+    echo "Username:       $username"
+    echo 
+    echo "Uptime:         $uptime"
+}
+
+printResourceInfo() {
+    echo "Disk usage:     $diskUsage"
+    echo
+    echo "CPU Usage:      $cpuUsage%" 
+    echo 
+    echo "Memory Usage:   $ramUsage"
+    echo 
+    echo -n -e  "Battery:        $batteryColour$batteryPercent%\e[0m "
+    createProgressBar "$batteryPercent"
+}
+
+printNetworkInfo() {
+    echo "IP address:     $ipAddress"
+}
+
+setBatteryColour() {
+    if [ "$batteryPercent" -ge 50 ]; then 
+        batteryColour="\e[32m"
+        
+    elif [ "$batteryPercent" -ge 20 ]; then 
+        batteryColour="\e[33m"
+        
+    else 
+        batteryColour="\e[31m"
+        
+    fi
+}
+
+createProgressBar() {
+    local filledBlocks=$(($1/10)) 
+    local emptyBlocks=$((10 - $filledBlocks))
+    for i in $(seq 1 $filledBlocks) 
+    do 
+        echo -n "⬛"
+    done
+    for i in $(seq 1 $emptyBlocks) 
+    do 
+        echo -n "⬜"
+    done
+    echo
+}
+
+main() {
+    getSysInfo
+    setBatteryColour
+    printHeader
+    echo 
+    echo "General"
+    echo "------------------------------------"
+    echo
+    printGeneralInfo
+    echo
+    echo "Resources"
+    echo "------------------------------------"
+    echo
+    printResourceInfo  
+    echo
+    echo "Network"
+    echo "------------------------------------"
+    echo 
+    printNetworkInfo
+}
+
+main
+
